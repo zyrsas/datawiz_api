d9e694747594250f6dd65f8abe946754eecd1ec3
Sasha <zyrsas@gmail.com>
2017-04-23 00:31:04 +0300

    added cookie auth

--- datawiz_api/urls.py
+++ datawiz_api/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from statistics import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.login),
+    url(r'^stat/$', views.stat),
 ]
--- statistics/views.py
+++ statistics/views.py
@@ -1,6 +1,28 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.contrib import auth
+from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
+from dwapi import datawiz, datawiz_auth
 
-from django.shortcuts import render
 
-# Create your views here.
+
+@csrf_exempt
+def login(request):
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            dw = datawiz.DW(username, password)
+            dw_info = dw.get_client_info()
+            request.session['username'] = username
+            request.session['password'] = password
+            request.session.save()
+            return HttpResponseRedirect('/stat/')
+        except ValueError:
+            return render_to_response("main.html", {'error': True})
+    return render_to_response("main.html", {'error': True})
+
+
+def stat(request):
+    return render_to_response("stat.html", {'user': request.session['username'],
+                                            'password': request.session['password']})
