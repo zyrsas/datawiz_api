cc8fd644e9eb8cca816265414cdd6a530e42b9ea
Sasha <zyrsas@gmail.com>
2017-04-23 11:40:54 +0300

    auth system update

--- statistics/views.py
+++ statistics/views.py
@@ -9,6 +9,7 @@ import pandas as pd
 
 @csrf_exempt
 def login(request):
+    error = False
     if request.POST:
         username = request.POST['username']
         password = request.POST['password']
@@ -19,19 +20,31 @@ def login(request):
             request.session.set_expiry(0)
             request.session.save()
             return HttpResponseRedirect('/stat/')
-        except ValueError:
-            return render_to_response("main.html", {'error': True})
-    return render_to_response("main.html", {'error': True})
+        except:
+            error = True
+            return render_to_response("main.html", {'error': error})
+    return render_to_response("main.html", {'error': error})
 
 
+@csrf_exempt
 def stat(request):
-    username = request.session['username']
-    password = request.session['password']
-    dw = datawiz.DW(username, password)
-    dw_sales = dw.get_categories_sale(date_from="2015-10-11", date_to="2015-10-15")
-    dw_info = dw.get_client_info()
-    df = pd.DataFrame(dw_sales)
-    df = df.to_html()
-    return render_to_response("stat.html", {'user': request.session['username'],
+    if request.method == "POST":
+        try:
+            del request.session['username']
+            del request.session['password']
+        except KeyError:
+            pass
+        return HttpResponseRedirect("/")
+    try:
+        username = request.session['username']
+        password = request.session['password']
+        dw = datawiz.DW(username, password)
+        dw_sales = dw.get_categories_sale(date_from="2015-10-11", date_to="2015-10-15")
+        dw_info = dw.get_client_info()
+        df = pd.DataFrame(dw_sales)
+        df = df.to_html()
+        return render_to_response("stat.html", {'user': request.session['username'],
                                             'password': request.session['password'],
                                             'dw_info': df})
+    except KeyError:
+        return render_to_response("permission.html")
