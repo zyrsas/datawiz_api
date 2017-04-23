a1a0b6afc99f31bec0de5f1c70eda8e740c67e8a
Sasha <zyrsas@gmail.com>
2017-04-23 10:48:16 +0300

    pandas cool

--- statistics/views.py
+++ statistics/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect
 from dwapi import datawiz, datawiz_auth
+import pandas as pd
 
 
 
@@ -13,9 +14,9 @@ def login(request):
         password = request.POST['password']
         try:
             dw = datawiz.DW(username, password)
-            dw_info = dw.get_client_info()
             request.session['username'] = username
             request.session['password'] = password
+            request.session.set_expiry(0)
             request.session.save()
             return HttpResponseRedirect('/stat/')
         except ValueError:
@@ -24,5 +25,13 @@ def login(request):
 
 
 def stat(request):
+    username = request.session['username']
+    password = request.session['password']
+    dw = datawiz.DW(username, password)
+    dw_sales = dw.get_categories_sale(date_from="2015-10-11", date_to="2015-10-15")
+    dw_info = dw.get_client_info()
+    df = pd.DataFrame(dw_sales)
+    df = df.to_html()
     return render_to_response("stat.html", {'user': request.session['username'],
-                                            'password': request.session['password']})
+                                            'password': request.session['password'],
+                                            'dw_info': df})
