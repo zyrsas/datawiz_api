f66a15be5fb0289a2ed2eb0a87aca4c1db210259
Sasha <zyrsas@gmail.com>
2017-04-24 12:09:27 +0300

    dw_func.  Client info/sales/shops

---
+++ statistics/dw_func.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import datetime
+
+
+date_from = "2015-11-17"
+date_to = "2015-11-18"
+
+
+def client_info(dw):
+    dw_info = dw.get_client_info()
+    dw_info = pd.DataFrame(dw_info)
+    return dw_info.to_html()
+
+
+def categories_sales(dw):
+    dw_sales = dw.get_categories_sale(date_from=date_from, date_to=date_to)
+    dw_sales = pd.DataFrame(dw_sales)
+    return dw_sales.to_html()
+
+
+def client_shops(dw):
+    dw_shops = dw.get_shops()
+    dw_shops = pd.DataFrame(dw_shops)
+    return dw_shops.to_html()
--- statistics/views.py
+++ statistics/views.py
@@ -1,15 +1,19 @@
-from django.contrib import auth
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect
 from dwapi import datawiz, datawiz_auth
-import pandas as pd
+import datetime
+from dw_func import client_info, categories_sales, client_shops
 
+
+# global DataWiz
+dw = datawiz.DW()
 
 
 @csrf_exempt
 def login(request):
     error = False
+    global dw
     if request.POST:
         username = request.POST['username']
         password = request.POST['password']
@@ -28,6 +32,7 @@ def login(request):
 
 @csrf_exempt
 def stat(request):
+    global dw
     if request.method == "POST":
         try:
             del request.session['username']
@@ -36,15 +41,25 @@ def stat(request):
             pass
         return HttpResponseRedirect("/")
     try:
+
         username = request.session['username']
         password = request.session['password']
-        dw = datawiz.DW(username, password)
-        dw_sales = dw.get_categories_sale(date_from="2015-10-11", date_to="2015-10-15")
-        dw_info = dw.get_client_info()
-        df = pd.DataFrame(dw_sales)
-        df = df.to_html()
-        return render_to_response("stat.html", {'user': request.session['username'],
-                                            'password': request.session['password'],
-                                            'dw_info': df})
+
+
+        """df = dw.get_products_sale(products=[2833024, 2286946, 'sum'], by='turnover',
+                              shops=[305, 306, 318, 321],
+                              date_from = datetime.date(2015, 11, 17),
+                              date_to = datetime.date(2015, 12, 17),
+                              interval = datawiz.WEEKS)"""
+
+        context = {'user': username,
+                   'password': password,
+                   'dw_info': client_info(dw),
+                   'dw_sales': categories_sales(dw),
+                   'dw_shops': client_shops(dw),
+                   #'dw_shops': dw_porduct,
+                  }
+
+        return render_to_response("stat.html", context)
     except KeyError:
         return render_to_response("permission.html")
